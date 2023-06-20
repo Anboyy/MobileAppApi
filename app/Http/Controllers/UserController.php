88e9dbe888dc14ad1eb190edbc044efae4d00a70
Anboyy <90738592+Anboyy@users.noreply.github.com>
2023-06-20 15:45:49 +0800

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function store(Request $request)
+    {
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        return response()->json(['message' => 'User created successfully']);
+    }
+}
